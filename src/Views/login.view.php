747ca672233df44d1e83f490b4f16b3012ad97ac
Ilia Abedian <eilia.abedian@yahoo.com>
2023-03-21 02:18:52 -0700

    password inputs should be hidden

--- src/Views/login.view.php
+++ src/Views/login.view.php
@@ -61,7 +61,7 @@
                     <label class="block text-[#F8F6F2] text-sm font-bold mb-2" for="password">
                         Password
                     </label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-[#6B705C] leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="text" placeholder="Password" maxlength="8" required>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-[#6B705C] leading-tight focus:outline-none focus:shadow-outline" id="password" name="password" type="password" placeholder="Password" maxlength="8" required>
                 </div>
                 <div class="flex items-center justify-between">
                     <button class="bg-[#6B705C] hover:bg-[#A5A58D] text-[#F8F6F2] font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
