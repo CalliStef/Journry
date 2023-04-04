6267c35f193b34d72bb14eb74853d6af7b2a2a29
Callista Stefanie Taswin <callistastefanie@gmail.com>
2023-04-04 14:50:44 -0700

    .env in activate.php

--- src/activate.php
+++ src/activate.php
@@ -3,7 +3,11 @@
 require_once('./Controllers/DbController.php');
 
 use \Controllers\DbController;
-$conn = DbController::create_connection("localhost", "php_term_project", "root", "Griya_indah49");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+$conn = DbController::create_connection($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"], $_ENV["DB_PORT"]);
 
 // get the activation token from the URL
 $token = $_GET['token'];
